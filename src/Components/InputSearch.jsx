097e0edbbc6f8638c320ab495b50f33aa498c264
Obed <obedestrada52@gmail.com>
2022-06-13 21:26:45 -0600

    Rickandmorty

--- src/Components/InputSearch.jsx
+++ src/Components/InputSearch.jsx
@@ -9,8 +9,6 @@ const InputSearch = ({setlocationSearch}) => {
 
   }
 
-
-
   return (
     <form className='search' onSubmit={SearchLocation}>
       
--- src/Components/LocationInfo.jsx
+++ src/Components/LocationInfo.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-
 const LocationInfo = ({location}) => {
   return (
   <article className='location-info'>
